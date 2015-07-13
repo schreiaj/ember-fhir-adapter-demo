9c7c548754b538c70aab945195f8e4a93667d8a2
Andrew Schreiber <aschreiber@va00029-pc.mitre.org>
2015-07-13 10:33:37 -0400

    Update demo for latest version of adapter

--- app/models/patient.js
+++ app/models/patient.js
@@ -2,9 +2,12 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   birthDate: DS.attr("date"),
-  name: DS.hasMany('human-name'),
+  name: DS.hasMany('human-name', {embedded: true}),
+  gender: DS.attr("string"),
+  text: DS.belongsTo("text", {embedded: true}),
+  conditions: DS.hasMany("condition", {async: true, searchBy: "patient"}),
 
   fullName: (function(){
-    return this.get('name').get('firstObject')
+    return this.get('name').get('firstObject');
   }).property('name')
 });
--- app/serializers/patient.js
+++
@@ -1,8 +0,0 @@
-import ApplicationSerializer from './application'
-
-export default ApplicationSerializer.extend({
-  attrs: {
-    name : {embedded: 'always'}
-  }
-
-});
